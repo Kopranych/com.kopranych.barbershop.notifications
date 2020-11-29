bfb8626197589b1cf2cbf2801dee641cb73cca50
Kopranych <kopranych@gmail.com>
2020-11-29 18:45:18 +0300

    add circuitBreaker

--- src/main/kotlin/com/kopranych/barbershop/notifications/client/ClientsClient.kt
+++ src/main/kotlin/com/kopranych/barbershop/notifications/client/ClientsClient.kt
@@ -2,52 +2,25 @@ package com.kopranych.barbershop.notifications.client
 
 import com.kopranych.barbershop.notifications.model.CustomPageImpl
 import com.kopranych.barbershop.notifications.model.dto.ClientDto
-import org.springframework.cloud.client.discovery.DiscoveryClient
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.data.domain.Page
 import org.springframework.http.HttpMethod
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
-import org.springframework.web.util.UriComponentsBuilder
-import java.lang.String
 
 
 @Component
-class ClientsClient(private val restTemplate: RestTemplate, private val discoveryClient: DiscoveryClient) {
+class ClientsClient(private val restTemplate: RestTemplate) {
 
-
-//  fun getAllClients(page: Int, size: Int): Page<ClientDto>? {
-//    val queryParam = UriComponentsBuilder
-//        .fromHttpUrl("http://clientsservice/api/clients")
-//        .queryParam("page", page)
-//        .queryParam("size", size)
-//    val response = restTemplate.exchange(
-//        "http://clientsservice/api/clients",
-//        HttpMethod.GET,
-//        null, object : ParameterizedTypeReference<Page<ClientDto>>() {}, queryParam
-//    )
-//    return response.body
-//  }
-
+  @HystrixCommand
   fun getAllClients(page: Int, size: Int): Page<ClientDto>? {
-    val restTemplate = RestTemplate()
-    val instances = discoveryClient.getInstances("clients")
-    if (instances.size == 0) return null
-
-    val serviceUri = String.format("%s/api/clients",
-        instances[0].uri.toString())
-    val queryParam = UriComponentsBuilder
-        .fromHttpUrl(serviceUri)
-        .queryParam("page", page)
-        .queryParam("size", size)
-
-
-    val restExchange: ResponseEntity<CustomPageImpl<ClientDto>> = restTemplate.exchange(
-        serviceUri,
+    val url = "http://clients/api/clients?page={page}&size={size}"
+    val response = restTemplate.exchange(
+        url,
         HttpMethod.GET,
-        null, object : ParameterizedTypeReference<CustomPageImpl<ClientDto>>() {}, queryParam)
-    return restExchange.body
+        null, object : ParameterizedTypeReference<CustomPageImpl<ClientDto>>() {}, page, size
+    )
+    return response.body
   }
-
 }
--- src/main/kotlin/com/kopranych/barbershop/notifications/config/CloudConfig.kt
+++ src/main/kotlin/com/kopranych/barbershop/notifications/config/CloudConfig.kt
@@ -1,12 +1,12 @@
 package com.kopranych.barbershop.notifications.config
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
-@EnableDiscoveryClient
+@EnableCircuitBreaker
 @Component
 class CloudConfig {
 
